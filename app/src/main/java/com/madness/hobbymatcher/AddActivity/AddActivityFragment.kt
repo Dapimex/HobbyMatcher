c73ffe734b884d3afa39eb44b8c122cf98d81b4f
Gewery <gewery@mail.ru>
2020-12-07 23:32:18 +0300

    Added adapter, updated fragment

--- app/src/main/java/com/madness/hobbymatcher/AddActivity/AddActivityFragment.kt
+++ app/src/main/java/com/madness/hobbymatcher/AddActivity/AddActivityFragment.kt
@@ -5,12 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.madness.hobbymatcher.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -24,34 +20,11 @@ class AddActivityFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_activity, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddActivityFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                AddActivityFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
     }
 }
---
+++ app/src/main/java/com/madness/hobbymatcher/adapter/ActivitiesAdapter.kt
@@ -0,0 +1,44 @@
+package com.madness.hobbymatcher.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.madness.hobbymatcher.R
+import com.madness.hobbymatcher.data.Activity
+
+class ActivitiesAdapter(private val activities: List<Activity>)
+    : RecyclerView.Adapter<ActivitiesAdapter.ActivityViewHolder>() {
+
+    class ActivityViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val nameTextView: TextView = view.findViewById(R.id.activityNameListItemTextView)
+        val descriptionTextView: TextView = view.findViewById(R.id.activityDescriptionListItemTextView)
+        val startTimeTextView: TextView = view.findViewById(R.id.activityStartTimeListItemTextView)
+        val locationTextView: TextView = view.findViewById(R.id.activityStartTimeListItemTextView)
+
+        fun bind(activity: Activity) {
+            nameTextView.text = activity.name
+            descriptionTextView.text = activity.description
+            startTimeTextView.text = activity.startTime
+            locationTextView.text = activity.location
+        }
+    }
+
+    private fun ViewGroup.inflate(layoutRes: Int, attachToRoot: Boolean = false): View {
+        return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActivityViewHolder {
+        return ActivityViewHolder(parent.inflate(R.layout.activity_list_item))
+    }
+
+    override fun getItemCount(): Int {
+        return activities.size
+    }
+
+    override fun onBindViewHolder(holder: ActivityViewHolder, position: Int) {
+        holder.bind(activities[position])
+    }
+
+}
