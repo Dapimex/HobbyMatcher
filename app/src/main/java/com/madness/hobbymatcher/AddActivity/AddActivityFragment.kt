c5a4f881b912904500a5fdd0cb5581c7852212d2
Denis Pimenov <d.pimenov@innopolis.ru>
2020-12-11 01:10:51 +0300

    Add Activity Fragment

--- app/src/main/java/com/madness/hobbymatcher/AddActivity/AddActivityFragment.kt
+++ app/src/main/java/com/madness/hobbymatcher/AddActivity/AddActivityFragment.kt
@@ -98,7 +98,6 @@ class AddActivityFragment : Fragment() {
                     Toast.makeText(context, "Successfully created activity", Toast.LENGTH_SHORT).show()
                     view?.findNavController()?.navigate(R.id.action_menu_item_add_activity_to_menu_item_profile)
                 } else {
-                    println(response)
                     Toast.makeText(context, "Failed to upload activity: ${response}", Toast.LENGTH_LONG).show()
                 }
             }
